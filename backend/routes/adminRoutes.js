5ec754878c9b401c10f52b963525eb5833753f4e
Udaykiran2310 <103480671+Udaykiran2310@users.noreply.github.com>
2025-05-11 20:18:37 +0530

    Delete backend directory

--- backend/routes/adminRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router()
-const {getUserData, updateUserStatus} = require('../controllers/adminController');
-
-router.get('/user', getUserData)
-router.patch('/user/status/:id', updateUserStatus)
-
-module.exports = router
--- backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router()
-const {getUserData, createUserData} = require('../controllers/userController');
-
-router.get('/', getUserData)
-router.post('/', createUserData)
-
-module.exports = router
